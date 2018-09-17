e3f94af8b8e14d4ab6e762796d441af28e49553b
Computadora Casa - Bryan <litcbryancg@hotmail.com>
2018-09-17 09:34:20 -0500

    separated the class from the commands

--- main.rb
+++ main.rb
@@ -1,34 +1,5 @@
-class TicTakToe
- @@player = true
-  def create_board(size)
-    board = Array.new(size,[])
-    return fill_board(board)
-  end
-  def fill_board(board)
-    for i in 0...board.length
-      for b in 0...board.length
-       board[i][b] = "1"
-      end
-    end
-    return board
-  end
-  def  select_position_board(board)
-       x = gets.chomp
-       y = gets.chomp
-       if board[x][y] = " "
-        if @@player == true
-         board[x][y] = "o"
-          @@player = false
-        else
-        end
-         board[x][y] = "x"
-         @@player = true
-       end
-  end
-end
-game = TicTakToe.new
+load "tictaktoe.rb"
 puts "Tell me size of the board:"
 size = gets.chomp
-size = size.to_i
-board = game.create_board(size)
-puts board
+game = TicTakToe.new(size)
+game.start
